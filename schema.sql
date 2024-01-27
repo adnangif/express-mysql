423315f6e364ac6289c9c63c500e85d8ddda1b17
Fahim Adnan <59190871+adnangif@users.noreply.github.com>
2024-01-27 12:46:23 +0600

    added schema

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE SCHEMA notesdb;
+USE notesdb;
+
+CREATE TABLE notes(
+id integer AUTO_INCREMENT,
+first_name VARCHAR(40),
+last_name VARCHAR(40) NOT NULL,
+age integer NOT NULL,
+PRIMARY KEY(id)
+
+);
